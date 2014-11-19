62fa6ca83595251173b97d3c59a0033eaeccb877
Kyle Frank <kyle.frank@mail.wvu.edu>
2014-11-19 13:49:38 -0500

    control panel index view changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141118214857) do
+ActiveRecord::Schema.define(:version => 20141029192131) do
 
   create_table "accounts", :force => true do |t|
     t.integer  "user_id"
@@ -45,11 +45,6 @@ ActiveRecord::Schema.define(:version => 20141118214857) do
   end
 
   add_index "clouds", ["account_id"], :name => "index_clouds_on_account_id"
-
-  create_table "control_panels", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
 
   create_table "users", :force => true do |t|
     t.string   "login",                  :default => "",    :null => false
